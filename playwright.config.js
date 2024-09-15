c958f12e4dac29a5053bd9c380a3417c6a7b4d55
Bosko Sretenovic <bosko.sretenovic@finiti.rs>
2024-09-15 18:40:05 +0200

    add github reporter

--- playwright.config.js
+++ playwright.config.js
@@ -21,11 +21,15 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
  // workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [ 
-    ['html', { open: 'never' }],
-    ['json', { outputFile: 'test-results/results.json' }],
-    ['junit', { outputFile: 'test-results/results.xml' }]
-    ],
+  reporter: [
+    ['list'],
+    ['@estruyf/github-actions-reporter']
+  ],
+  // reporter: [ 
+  //   ['html', { open: 'never' }],
+  //   ['json', { outputFile: 'test-results/results.json' }],
+  //   ['junit', { outputFile: 'test-results/results.xml' }]
+  //   ],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   retries: 2,
